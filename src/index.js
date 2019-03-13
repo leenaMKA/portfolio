17ea4ec2a853eae093eb4e411ee84f941ed95398
Leena Mkarem <leena@leenas-mbp.intern>
2019-03-13 14:08:34 +0100

    First step

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import Popper from "popper.js";
-require('bootstrap');
+require("bootstrap");
+
+var $root = $("html, body");
+var navMainHeight = $(".nav-main").outerHeight();
+
+$('a[href^="#"]').click(function(e) {
+  e.preventDefault();
+  var href = $.attr(this, "href");
+
+  $root.animate(
+    {
+      scrollTop: $(href).offset().top - navMainHeight
+    },
+    1000
+  );
+
+  return false;
+});
