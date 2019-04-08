4d5ed7c4ada4d7affe6c19d1e4a967e39cbe0b0a
Leena Mkarem <leena.mkarem@gmail.com>
2019-04-08 17:27:49 +0200

    Validate form control

--- src/index.js
+++ src/index.js
@@ -4,31 +4,6 @@ require("bootstrap");
 const $root = $("html, body");
 const navMainHeight = $(".nav-main").outerHeight();
 
-$("#send-id").click(function() {
-  let newMessage = {
-    name: $("#name").val(),
-    email: $("#email").val(),
-    message: $("message").val()
-  };
-  $.ajax({
-    url: "http://please ",
-    type: "POST",
-    data: JSON.stringify(newMessage),
-    dataType: "json",
-    contentType: "application/json",
-    success: function(response) {
-      console.log("response", response);
-    },
-    error: function(xhr, status, error) {
-      console.log(`
-        error:${error},
-        status:${status},
-        xhr:${JSON.stringify(xhr)}
-        `);
-    }
-  });
-});
-
 $('a[href^="#"]').click(function(e) {
   e.preventDefault();
   console.log($.attr(this, "href"));
@@ -44,6 +19,9 @@ $('a[href^="#"]').click(function(e) {
   return false;
 });
 
-//create the box, make it width 100, easy to click on
-// create the angle itself
-// when click on the angle, jump to the first next section
+$(document).ready(function() {
+  $(".submit").click(function(e) {
+    event.preventDefault();
+    console.log("clickid");
+  });
+});
